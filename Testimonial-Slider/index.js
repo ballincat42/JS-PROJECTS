c4a4516cfd4e6958b4477501d816632e888696d7
BallinCat42 <jonhnevibel@gmail.com>
2024-06-22 13:50:24 +0700

    add an array objects of cars and clean up the html and css

---
+++ Testimonial-Slider/index.js
@@ -0,0 +1,17 @@
+const Cars = [
+    {
+        Name:"Axle",
+        Text:"known as silly axle, very silly car, can't even form a thought😐",
+        Image:"/Testimonial-Slider/img/axle.png",
+    },
+    {
+        Name:"Blawg",
+        Text:"another version of silly axle, a very innocent car to ever exist, blawg is a CutiePatootie😊",
+        Image:"/Testimonial-Slider/img/blawg.png",
+    },
+    {
+        Name:"Schlawg",
+        Text:"one of the lookxmaxing car founder, such a sigma car, Chad😐",
+        Image:"/Testimonial-Slider/img/axle.png",
+    },
+]
