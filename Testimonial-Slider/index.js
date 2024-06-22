c844dd00ca92e776ab053b2b3a36f663bd66039b
BallinCat42 <jonhnevibel@gmail.com>
2024-06-22 14:24:42 +0700

    finish the js code and cleaning everything up:3

--- Testimonial-Slider/index.js
+++ Testimonial-Slider/index.js
@@ -1,17 +1,44 @@
 const Cars = [
     {
-        Name:"Axle",
+        NameCar:"Beluga",
+        Text:"The yt car,Looks a bit silly and innocent😐",
+        Image:"/Testimonial-Slider/img/beluga.png",
+    },
+    {
+        NameCar:"Axle",
         Text:"known as silly axle, very silly car, can't even form a thought😐",
         Image:"/Testimonial-Slider/img/axle.png",
     },
     {
-        Name:"Blawg",
+        NameCar:"Blawg",
         Text:"another version of silly axle, a very innocent car to ever exist, blawg is a CutiePatootie😊",
-        Image:"/Testimonial-Slider/img/blawg.png",
+        Image:"/Testimonial-Slider/img/blawg.jpg",
     },
     {
-        Name:"Schlawg",
+        NameCar:"Schlawg",
         Text:"one of the lookxmaxing car founder, such a sigma car, Chad😐",
         Image:"/Testimonial-Slider/img/axle.png",
     },
-]
+];
+
+const NameEL = document.querySelector('.Name');
+const ImageEL = document.querySelector('img');
+const TextEL = document.querySelector('.Text');
+
+let idx = 0;
+
+updateCars()
+
+function updateCars(){
+    const {NameCar,Image,Text} = Cars[idx];
+    NameEL.innerText = NameCar;
+    ImageEL.src = Image;
+    TextEL.innerText = Text;
+    idx++;
+    if(idx === Cars.length){
+        idx = 0;
+    }
+    setTimeout(() => {
+        updateCars();
+      }, 3000);
+}
