1a6b2742577818df73265e9e9ed21fd27a0fcf3c
BallinCat42 <jonhnevibel@gmail.com>
2024-07-02 20:17:46 +0700

    Aww~ i did it! i just a few minutes:3

--- RandomColor-Generator/index.js
+++ RandomColor-Generator/index.js
@@ -1,10 +1,27 @@
-
+const containerEl = document.querySelector('.container');
 //generate the boxes
+for(let i = 0; i < 30; i++){
+    const boxes = document.createElement('div');
+    boxes.classList.add('Rcontainer');
+    containerEl.appendChild(boxes);
+}
+const boxesEls = document.querySelectorAll('.Rcontainer');
 
 //append the color
-
+appendColor()
+function appendColor(){
+    boxesEls.forEach((boxesEl) => {
+        const rColor = generateColor();
+        boxesEl.style.backgroundColor = rColor;
+        boxesEl.innerText = rColor;
+    })
+}
 
 //generate the random color
-
+function generateColor(){
+    let RandomColor = Math.floor(Math.random() * 16777215);
+    RandomColor = `#${RandomColor.toString(16).padStart(6,'0')}`
+    return RandomColor
+}
 
 // 16777215 = FFFFFF in hex color value
